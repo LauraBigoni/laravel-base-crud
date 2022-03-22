6dbe815fa4b5c1d63bdbdc97fa402774cd5c93f6
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-22 15:37:42 +0100

    added header with includes in main layout

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">COMICS |</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comics</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+    @include('includes.header')
     
     <main>
 
